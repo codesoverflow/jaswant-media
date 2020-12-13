e7fb372f68cfb0cf708c48142ad9c90d9e379209
Jaswant Mandloi <jaswantmandloi@gmail.com>
2020-12-13 13:10:56 +0530

    Add media thumbnails

--- jaswant-media.php
+++ jaswant-media.php
@@ -9,7 +9,9 @@
    License: GPL2
    */
 
-   require_once 'config.php';
+$isCdnContentEnabled = defined("WP_CONTENT_URL");
+
+$jmCdnImgUrl = $isCdnContentEnabled ? WP_CONTENT_URL : '';
    
 
    
@@ -24,51 +26,87 @@
 
 //apply_filters( 'wp_calculate_image_sizes', $sizes, $size, $image_src, $image_meta, $attachment_id );
 
+//$sources = apply_filters( 'wp_calculate_image_srcset', $sources, $size_array, $image_src, $image_meta, $attachment_id );
    
 
-   add_filter('wp_get_attachment_url', 
+//the_post_thumbnail( 'small-thumb' );    
+
+// if ( !extension_loaded('gd') || !function_exists('gd_info') ) {
+//     echo 'GD Library Not Installed';
+// } else {
+//     echo 'GD Library Available';
+// }
+
+// die;
+
+
+
+$thumbnailsToGenerate = array(
+    'original'=> array(
+        'height' => 500,
+        'width' => 500
+    ),
+    'thumbnail' => array(
+        'height' => 150,
+        'width' => 150
+    ),
+    'td_80x60'=> array(
+        'height' => 80,
+        'width' => 60
+    ),
+    'td_100x70'=> array(
+        'height' => 100,
+        'width' => 70
+    ),
+);
+
+foreach($thumbnailsToGenerate as $thumbKey => $thumbnailToGenerate) {
+    add_image_size($thumbKey, $thumbnailToGenerate['height'], $thumbnailToGenerate['width'], true );
+}
+
+
+
+if($isCdnContentEnabled) {
+
+    add_filter('wp_get_attachment_url', 
     'wp_get_attachment_url_jm', 10, 3);
 
 
     add_filter('wp_calculate_image_srcset', 
     'wp_calculate_image_srcset_jm', 10, 3);
+}
 
-    //$sources = apply_filters( 'wp_calculate_image_srcset', $sources, $size_array, $image_src, $image_meta, $attachment_id );
+    
 
 
-    function wp_calculate_image_srcset_jm($sources) {
-        
-        
-        foreach($sources as $sourceKey => $source) {
-            $updateUrl = jm_replace_image_url($source['url']);
-            $sources[$sourceKey]['url'] = $updateUrl;
-        }
-
-        return $sources;
+function wp_calculate_image_srcset_jm($sources) {
+    foreach($sources as $sourceKey => $source) {
+        $updateUrl = jm_replace_image_url($source['url']);
+        $sources[$sourceKey]['url'] = $updateUrl;
     }
 
+    return $sources;
+}
 
-    function wp_get_attachment_url_jm($url, $post_id) {
 
-        return jm_replace_image_url($url);
+function wp_get_attachment_url_jm($url, $post_id) {
+
+    return jm_replace_image_url($url);
+}
+
+function jm_replace_image_url($url) {
+    global $jmCdnImgUrl;
+
+    if(is_admin()) {
+        return $url;
     }
 
-    function jm_replace_image_url($url) {
-        global $jmCdnImgUrl;
-
-        if(is_admin()) {
-            return $url;
-        }
-
-        $imgUrlArr = explode('/wp-content/', $url);
-        if(count($imgUrlArr) != 2 ) {
-            return $url;
-        }
-
-        $imgSecondPart = $imgUrlArr[1];
-
-        return $jmCdnImgUrl.$imgSecondPart;
+    $imgUrlArr = explode('/wp-content/', $url);
+    if(count($imgUrlArr) != 2 ) {
+        return $url;
     }
-   
 
-?>
+    $imgSecondPart = $imgUrlArr[1];
+
+    return $jmCdnImgUrl.'/'.$imgSecondPart;
+}
